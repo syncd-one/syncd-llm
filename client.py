bebe4ff9a7aee9498302d9a78469451596ea9ac1
Abhir Karande <abhir511@gmail.com>
2024-03-02 18:59:02 -0500

    documents class created

---
+++ client.py
@@ -0,0 +1,119 @@
+import cohere
+import os
+import hnswlib
+import json
+import json
+import uuid
+from typing import List, Dict
+from unstructured.partition.html import partition_html
+from unstructured.chunking.title import chunk_by_title
+
+co =  cohere.Client(os.environ['g8rkyTzW6QwzXaRDBQCVKIClLG8RfmP06GQZibdn'])
+
+#https://txt.cohere.com/rag-chatbot/#step-by-step-guide
+
+class Documents:
+
+    def __init__(self, sources: List[Dict[str, str]]):
+        self.sources = sources
+        self.docs = []
+        self.docs_embs = []
+        self.retrieve_top_k = 10
+        self.rerank_top_k = 3
+        self.load()
+        self.embed()
+        self.index()
+
+
+    def load(self) -> None:
+        """
+        Loads the documents from the sources and chunks the HTML content.
+        """
+        print("Loading documents...")
+
+        for source in self.sources:
+            elements = partition_html(url=source["url"])
+            chunks = chunk_by_title(elements)
+            for chunk in chunks:
+                self.docs.append(
+                    {
+                        "title": source["title"],
+                        "text": str(chunk),
+                        "url": source["url"],
+                    }
+                )
+
+    def embed(self) -> None:
+        """
+        Embeds the documents using the Cohere API.
+        """
+        print("Embedding documents...")
+
+        batch_size = 90
+        self.docs_len = len(self.docs)
+
+        for i in range(0, self.docs_len, batch_size):
+            batch = self.docs[i : min(i + batch_size, self.docs_len)]
+            texts = [item["text"] for item in batch]
+            docs_embs_batch = co.embed(
+		              texts=texts,
+                      model="embed-english-v3.0",
+                      input_type="search_document"
+	 		).embeddings
+            self.docs_embs.extend(docs_embs_batch)
+
+    def index(self) -> None:
+        """
+        Indexes the documents for efficient retrieval.
+        """
+        print("Indexing documents...")
+
+        self.index = hnswlib.Index(space="ip", dim=1024)
+        self.index.init_index(max_elements=self.docs_len, ef_construction=512, M=64)
+        self.index.add_items(self.docs_embs, list(range(len(self.docs_embs))))
+
+        print(f"Indexing complete with {self.index.get_current_count()} documents.")
+
+    def retrieve(self, query: str) -> List[Dict[str, str]]:
+        """
+        Retrieves documents based on the given query.
+
+        Parameters:
+        query (str): The query to retrieve documents for.
+
+        Returns:
+        List[Dict[str, str]]: A list of dictionaries representing the retrieved  documents, with 'title', 'snippet', and 'url' keys.
+        """
+        docs_retrieved = []
+        query_emb = co.embed(
+                    texts=[query],
+                    model="embed-english-v3.0",
+                    input_type="search_query"
+                    ).embeddings				    
+
+        doc_ids = self.index.knn_query(query_emb, k=self.retrieve_top_k)[0][0]
+        docs_to_rerank = []
+        for doc_id in doc_ids:
+            docs_to_rerank.append(self.docs[doc_id]["text"])
+
+        rerank_results = co.rerank(
+            query=query,
+            documents=docs_to_rerank,
+            top_n=self.rerank_top_k,
+            model="rerank-english-v2.0",
+        )
+
+        doc_ids_reranked = []
+        for result in rerank_results:
+            doc_ids_reranked.append(doc_ids[result.index])
+
+        for doc_id in doc_ids_reranked:
+            docs_retrieved.append(
+                {
+                    "title": self.docs[doc_id]["title"],
+                    "text": self.docs[doc_id]["text"],
+                    "url": self.docs[doc_id]["url"],
+                }
+            )
+
+        return docs_retrieved
